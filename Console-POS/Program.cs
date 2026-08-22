6afe1fb8a784e0deb7f4a6d3748b0895d925ed75
SammerLegend <baliguatsammer@gmail.com>
2026-08-22 21:52:33 +0800

    Add Display Menu

--- Console-POS/Program.cs
+++ Console-POS/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Console_POS
-
 {
     internal class Program
     {
@@ -18,18 +18,16 @@ namespace Console_POS
         private static void DisplayMenu()
         {
             Console.Clear();
-            Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("          Welcome to the POS System          ");
-            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("    Welcome to the POS System  ");
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("[1] Add Item");
             Console.WriteLine("[2] Remove Item");
             Console.WriteLine("[3] View Cart");
             Console.WriteLine("[4] Checkout");
-            Console.WriteLine("[5] exit");
-            Console.WriteLine("---------------------------------------------");
-            Console.Write("Please select an option");
-
+            Console.WriteLine("[5] Exit");
+            Console.WriteLine("==============================");
+            Console.Write("Please select an option: ");
         }
     }
-
 }
